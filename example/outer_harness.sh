5fb2c6bcd338c0b309a37f505dd8bbf509575ded
Edd Barrett <vext01@gmail.com>
2026-02-24 14:49:05 +0000

    Fix output on error in the example.

--- example/outer_harness.sh
+++ example/outer_harness.sh
@@ -11,12 +11,20 @@ bmark=$1; shift
 inproc_iters=$1; shift
 param=$1; shift
 
+set +e
 output=$("$executor" inner_harness.sh "$bmark" "$inproc_iters" "$param" 2>&1)
 s=$?
+set -e
 
 # shellcheck disable=SC2181
 if [ $s -ne 0 ]; then
     echo "error: failed to run inner harness"
+    # Print the output on failure, for debugging purposes.
+    #
+    # Ideally we'd capture stdout/stderr separately and re-output them back to
+    # the parent process' stdout/stderr, but that's not easy in portable shell
+    # (without using temp files).
+    echo "$output"
     exit $s
 fi
 
